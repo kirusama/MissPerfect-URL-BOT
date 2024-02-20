e4c3b6a57dd0a55cbcb380a826b831b77eadb34c
gkiran <gouthamkiran@a-zing.org>
2024-02-20 21:50:30 +0530

    working  code

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = "1646107601:AAGYliNs6gfj6tYf57fEXoFKx9wnOvXXuN0"
     # The Telegram API things
-    API_ID = int(os.environ.get("API_ID", ""))
-    API_HASH = os.environ.get("API_HASH","")
+    API_ID = "1638897"
+    API_HASH = "f970b41ffe6560473fd9da2bff560af2"
     # Get these values from my.telegram.org
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = "./DOWNLOADS"
@@ -24,9 +24,9 @@ class Config(object):
     # set timeout for subprocess
     PROCESS_MAX_TIMEOUT = 3600
     # your telegram id
-    OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+    OWNER_ID = "262397834"
     SESSION_NAME = "UPLOADER-X-BOT"
     # database uri (mongodb)
-    DATABASE_URL = os.environ.get("DATABASE_URL", "")
+    DATABASE_URL = "mongodb+srv://gouthakiran1997:gP22LRgxsZaS76Q2@cluster0.g8bzsjh.mongodb.net/?retryWrites=true&w=majority"
     MAX_RESULTS = "50"
     PREMIUM_USER = os.environ.get("PREMIUM_USER")
